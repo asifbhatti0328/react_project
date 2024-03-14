07669bd662320b75a042eb9bb2008c21041e690c
asifbhatti0328 <asifbhattiwebdeveloper@gmail.com>
2024-03-14 17:14:02 +0000

    aaaaaa

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'; 
 import './style/style.css'
+import './style/responsive.css'
+
 import App from './App';
 import { BrowserRouter } from 'react-router-dom' ;
 
